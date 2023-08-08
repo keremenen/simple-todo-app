bf6556d50d8feddde145dec5bd52acf09cf7397b
Kemer <kitowski.przemek@gmail.com>
2023-08-08 11:58:16 +0200

    Displaying a temporary todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const temporaryTodos = [
+  {
+    title: 'Take out the garbage',
+    id: Math.floor(Math.random() * 1000)
+  },
+  {
+    title: 'Do shopping',
+    id: Math.floor(Math.random() * 1000)
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(temporaryTodos)
 
   return (
-    <>
-      Hello world
-    </>
+    <div className="todo-app">
+      {todos && todos.map(todo => (
+        <li key={todo.id}>
+          <span>{todo.title}</span>
+        </li>
+      ))}
+    </div>
   )
 }
 
